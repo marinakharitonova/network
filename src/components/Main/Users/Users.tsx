6cc309c1b1e33ff9d91f59e6122a42c9fdcc2fae
Marina <6172331@mail.ru>
2023-02-20 19:50:21 +0300

    feat: set query params for users url

--- src/components/Main/Users/Users.tsx
+++ src/components/Main/Users/Users.tsx
@@ -1,16 +1,29 @@
 import UsersList from "./UsersList/UsersList";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PaginationApp from "../../PaginationApp/PaginationApp";
 import {useGetUsersQuery} from "../../../redux/features/api/apiSlice";
 import ContentLoader from "../../ContentLoader/ContentLoader";
+import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom";
 
 const Users = (): JSX.Element => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize] = useState(10);
+    let [searchParams, setSearchParams] = useSearchParams();
+    const queryPage = Number(searchParams.get('page'))
+    const queryCount = Number(searchParams.get('count'))
+
+    const [currentPage, setCurrentPage] = useState(queryPage ?? 1);
+    const [pageSize] = useState(queryCount ?? 10);
+
+    useEffect(() => {
+        if (currentPage !== queryPage) {
+            setCurrentPage(queryPage);
+        }
+
+    }, [queryPage, queryCount])
 
     const {
         data,
         isLoading,
+        isFetching,
         isSuccess,
         isError,
         error
@@ -18,12 +31,14 @@ const Users = (): JSX.Element => {
 
     const handlePageChange = (current: number) => {
         setCurrentPage(current);
+
+        setSearchParams({page: current.toString(), count: pageSize.toString()})
     }
 
     return (
         <ContentLoader isError={isError} isLoading={isLoading} isSuccess={isSuccess} error={error}>
             {data && <>
-                <UsersList users={data.users} page={currentPage} pageSize={pageSize}/>
+                <UsersList users={data.users} page={currentPage} pageSize={pageSize} isFetching={isFetching}/>
                 <PaginationApp total={data.totalCount} pageSize={pageSize}
                                current={currentPage}
                                handler={handlePageChange}/>
--- src/components/Main/Users/UsersList/UsersList.tsx
+++ src/components/Main/Users/UsersList/UsersList.tsx
@@ -4,16 +4,20 @@ import UserItem from "./UserItem/UserItem";
 interface UsersListProps {
     users: IUser[],
     page: number,
-    pageSize: number
+    pageSize: number,
+    isFetching: boolean
 }
 
-const UsersList = ({users, page, pageSize}: UsersListProps) => {
+const UsersList = ({users, page, pageSize, isFetching}: UsersListProps) => {
+
+    const style = isFetching ? {opacity: 0.5, transition: 'opacity 0.3s'} : {opacity: 1, transition: 'opacity 0.3s'}
 
     return (
         <List
             itemLayout="horizontal"
             dataSource={users}
             renderItem={user => (<UserItem user={user} page={page} pageSize={pageSize}/>)}
+            style={style}
         />
     )
 }
